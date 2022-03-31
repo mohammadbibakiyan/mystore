38f2bc7162f30297f367117fc16da093b301d9f1
mohammad <mohammadbibakiyan@gmail.com>
2022-03-31 21:46:45 +0430

    404 page design complited

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 const NotFoundPage=()=>{
     return(
-        <h1>404</h1>
+        <div>
+            <img src="/images/404/404.png" className="border-0 w-full max-w-3xl mx-auto"/>
+            <div className="text-center">
+                <h2 className="text-h4">صفحه مورد نظر پیدا نشد</h2>
+                <Link href="/">
+                    <a className="text-button-2 text-blue-400"><span className="ml-5">صفحه اصلی</span><span>&gt;</span></a>
+                </Link>
+            </div>
+        </div>
     )
 }
 
