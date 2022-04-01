9b89d709c8f326d5efa987e0c8a6226133b0c011
mohammad <mohammadbibakiyan@gmail.com>
2022-04-01 15:16:02 +0430

    header design complited

--- component/layout/layout.js
+++ component/layout/layout.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 const Layout=(props)=>{
     return (
         <>
-        <h1>header</h1>
+        <nav className="flex justify-between items-center p-5">
+            <div>
+                <ul className="flex gap-8 items-center text-body-2">
+                    <li className="text-body1-strong">دسته بندی کالا ها</li>
+                    <li className="hover:text-red-500"><Link href="/">پرفروش ترین ها</Link></li>
+                    <li className="hover:text-red-500"><Link href="/">تخفیف ها و پیشنهادها</Link></li>
+                    <li className="hover:text-red-500"><Link href="/">شگفت انگیزها</Link></li>
+                    <li className="hover:text-red-500"><Link href="/">سوالی دارید</Link></li>
+                </ul>
+            </div>
+            <div className="flex gap-5">
+                <Link href="users/login"><a className="hover:cursor-pointer"><img src="/icons/login.svg"/></a></Link>
+                <Link href="cart"><a className="hover:cursor-pointer"><img src="/icons/shopping-cart.svg"/></a></Link>
+            </div>
+        </nav>
         {props.children}
         </>
     )
